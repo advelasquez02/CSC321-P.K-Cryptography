ad7561852f5ec14c72f6e3e1456fb28aaa0a79b9
advelasquez02 <advelasquez02@gmail.com>
2026-04-30 16:18:52 -0700

    Implemented MITM attack.

---
+++ MITM-Attack.py
@@ -0,0 +1,82 @@
+import random
+from Crypto.Hash import SHA256
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+
+q = int(
+    "B10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C6"
+    "9A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C0"
+    "13ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD70"
+    "98488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0"
+    "A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708"
+    "DF1FB2BC2E4A4371", 16
+)
+
+a = int(
+    "A4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507F"
+    "D6406CFF14266D31266FEA1E5C41564B777E690F5504F213"
+    "160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1"
+    "909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28A"
+    "D662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24"
+    "855E6EEB22B3B2E5", 16
+)
+# q,a = 37, 5
+IV= get_random_bytes(16)
+
+def alice(y_b):
+    # random integer for exponent in the range 2 to q-1
+    x_a = random.randint(1, q - 1)
+
+    # compute public key to send to bob
+    y_a = pow(a, x_a, q)
+
+    # compute shared secret
+    s = pow(y_b, x_a, q)
+
+    # hash the key and truncate to 16 bits
+    k = SHA256.new(str(s).encode()).digest()[:16]
+
+    # raw byte string
+    cipherTxt = AES.new(k, AES.MODE_CBC, IV).encrypt(pad(b"Hi Bob!", 16))
+    return y_a, cipherTxt
+
+def bob(y_a):
+    # random integer for exponent in the range 2 to q-1
+    x_b = random.randint(1, q - 1)
+    # compute public key to send to alice
+    y_b = pow(a, x_b, q)
+
+    # compute shared secret
+    s = pow(y_a, x_b, q)
+    # hash the key and truncate to 16 bits
+    k = SHA256.new(str(s).encode()).digest()[:16]
+
+    # raw byte string
+    cipherTxt = AES.new(k, AES.MODE_CBC, IV).encrypt(pad(b"Hi Alice!", 16))
+    return y_b, cipherTxt
+
+def mallroy(cipherTxt):
+    bob(q)
+    alice(a)
+
+    #since we know q and a, and the fact that we sent q to both alice and bob instead of
+    #their respective computed keys we know that s will always equal 0.
+    mallroy_s = pow(q, 1, q)
+    k = SHA256.new(str(mallroy_s).encode()).digest()[:16]
+    plaintext = unpad(AES.new(k, AES.MODE_CBC, IV).decrypt(cipherTxt), 16)
+    return plaintext.decode()
+
+
+
+if __name__ == '__main__':
+    # Mallory intercepts and sends q to both alice and bob instead of y_b and y_a
+    y_a, alice_txt = alice(q)
+    y_b, bob_txt = bob(q)
+
+    plaintext_a = mallroy(alice_txt)
+    plaintext_b = mallroy(bob_txt)
+
+    # note that we are not checking for the same keys. It was checked before.
+    # So we are essentially trusting the key generator since they are private.
+    print("Mallroy got the plaintexts:" + plaintext_a + plaintext_b)
